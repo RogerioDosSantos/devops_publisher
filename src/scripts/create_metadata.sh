0ad7ae9ae1f7b6c340335e3436a04d95a5585f29
Roger <roger.santos.main@gmail.com>
2018-05-22 17:47:33 -0500

    Created the functionality to create metadata

--- src/scripts/create_metadata.sh
+++ src/scripts/create_metadata.sh
@@ -60,6 +60,8 @@ DisplayHelp()
   echo "--log_enable (-le) : Enable log"
   echo "--log_level (-ll) <level>: Define the Log Level (Default: ${config_log_level})"
   echo "--log_type (-lt) <type>: Define the Log Type [Options: all, error, warning, info] (Default: ${config_log_type})"
+  echo "--location (-lc) <location_of_metadata>: Define the location from where the metadata will be generated from. (Default: ${config_location})"
+  echo "--output (-o) <file_path>: File that the result will be saved. (Default: Does not save)"
   echo " "
 }
 
@@ -68,6 +70,8 @@ GetConfiguration()
   config_log_enabled="0"
   config_log_level="1"
   config_log_type="all"
+  config_location="${g_caller_dir}"
+  config_output_file_path=""
   while [[ $# != 0 ]]; do
       case $1 in
           --log_enable|-le)
@@ -83,6 +87,16 @@ GetConfiguration()
           --log_type|-lt)
             config_log_type="$2"
             Log "info" "1" "Log Type" "${config_log_type}"
+            shift 2
+            ;;
+          --location|-lc)
+            config_location="$2"
+            Log "info" "1" "Location" "${config_location}"
+            shift 2
+            ;;
+          --output|-o)
+            config_output_file_path="$2"
+            Log "info" "1" "Output" "${config_output_file_path}"
             shift 2
             ;;
           --)
@@ -107,8 +121,60 @@ GetConfiguration()
 
 MainFunction()
 {
-  Log "info" "1" "TODO" "Add logic here"
-  echo "Function called"
+  Log "info" "1" "Creating the id" ""
+
+  cd "${g_caller_dir}"
+  cd "${config_location}"
+  config_location="$(pwd)"
+
+  local module_dir="$(git rev-parse --show-toplevel)"
+  local name="${module_dir##*/}"
+  local version="$(git rev-parse --short HEAD)"
+  local commit="$(git rev-parse HEAD)"
+  local branch="$(git rev-parse --abbrev-ref HEAD)"
+  local timestamp="$(git log -1 --date=iso --pretty=format:%cd)"
+  local location="${config_location/${module_dir}/}"
+
+  cd "${module_dir}/.."
+  module_dir="$(pwd)"
+  echo "$module_dir"
+
+  local upper_module_dir="$(git rev-parse --show-toplevel)"
+  local upper_name="${upper_module_dir##*/}"
+  local upper_version="$(git rev-parse --short HEAD)"
+  local upper_commit="$(git rev-parse HEAD)"
+  local upper_branch="$(git rev-parse --abbrev-ref HEAD)"
+  local upper_timestamp="$(git log -1 --date=iso --pretty=format:%cd)"
+  local upper_location="${module_dir/${upper_module_dir}/}"
+
+  local full_name="${upper_name}-${name}-${location}"
+  local full_version="${upper_branch}-${upper_version}-${branch}-${version}"
+  full_name=${full_name/\//-}
+  full_name=${full_name/--/-}
+  full_version=${full_version/--/-}
+
+  json_ret="{ 
+  "\"name\"":"\"${name}\"",
+  "\"version\"":"\"${version}\"",
+  "\"commit\"":"\"${commit}\"",
+  "\"branch\"":"\"${branch}\"",
+  "\"timestamp\"":"\"${timestamp}\"",
+  "\"location\"":"\"${location}\"",
+  "\"upperName\"":"\"${upper_name}\"",
+  "\"upperVersion\"":"\"${upper_version}\"",
+  "\"upperCommit\"":"\"${upper_commit}\"",
+  "\"upperBranch\"":"\"${upper_branch}\"",
+  "\"upperTimeStamp\"":"\"${upper_timestamp}\"",
+  "\"upperLocation\"":"\"${upper_location}\"",
+  "\"fullName\"":"\"${full_name}\"",
+  "\"fullVersion\"":"\"${full_version}\""
+}"
+
+  echo "${json_ret}"
+  if [ ${config_output_file_path} != "" ]; then 
+    cd "${g_caller_dir}"
+    echo "${json_ret}" > ${config_output_file_path}
+  fi
 }
 
 # Main
